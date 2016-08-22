76c1976de8564d41053c22108d01b8b3f2f41ed9
chelsey.lotts <chellybellie@gmail.com>
2016-08-22 12:24:57 -0700

    added more text

---
+++ Hello AIE/main.cpp
@@ -0,0 +1,15 @@
+//
+// Chelsey Lotts <chelsey.lotts@students.aie.edu.au>
+//
+
+#include <iostream>
+
+int main()
+{
+	printf("hello AIE my name is chelsey and i like cookies"); // prints text to window
+	printf("but i looooooooooove chocolate cookies");
+	
+
+	getchar(); // hold window open
+
+}
